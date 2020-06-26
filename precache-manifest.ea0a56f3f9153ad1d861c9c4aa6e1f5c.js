d3e17c03ef7e7a4d849e85d3f72dbdc2ffa26fb1
César Damasceno <64749161+ManeMaria@users.noreply.github.com>
2020-06-25 23:22:46 -0300

    Updates

---
+++ precache-manifest.ea0a56f3f9153ad1d861c9c4aa6e1f5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612f9e42f593614efa04c5f507f8be8b",
+    "url": "/ManeMaria/test.git/index.html"
+  },
+  {
+    "revision": "7dae51ecd3f4e55ac081",
+    "url": "/ManeMaria/test.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4a3286a154d77deabeb",
+    "url": "/ManeMaria/test.git/static/js/2.53de6e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ManeMaria/test.git/static/js/2.53de6e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dae51ecd3f4e55ac081",
+    "url": "/ManeMaria/test.git/static/js/main.dbf30390.chunk.js"
+  },
+  {
+    "revision": "e1df9dd9a49b787020c8",
+    "url": "/ManeMaria/test.git/static/js/runtime-main.90c46234.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ManeMaria/test.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
